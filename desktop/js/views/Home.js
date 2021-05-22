c08f103e85c3ca08ffdefc5ec1c099b8ba40b880
Antony <allinto@icloud.com>
2021-05-22 19:24:53 +1200

    styling + menu

--- desktop/js/views/Home.js
+++ desktop/js/views/Home.js
@@ -79,40 +79,33 @@ class Home extends View{
 }
 
 
-.dropbtn {
-  background-color: #f5f5f5;
-  color: white;
-  padding: 16px;
-  font-size: 16px;
-  border: none;
-  cursor: pointer;
-}
 
 .dropdown {
   position: relative;
   display: inline-block;
-}
+  width: 2em !important;
 
-.dropdown-content a{
-  background-color: #f5f5f5;
-  padding: 0em;
 }
 
 .dropdown-content {
   display: none;
   position: absolute;
   background-color: #f5f5f5;
-  z-index: 1;
-  left: 0em !important;
-  box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+  z-index: 103;
+  left: 0px;
+  padding: 5px;
+
 }
 
 .dropdown-content a {
   color: black;
   text-decoration: none;
   display: block;
-    padding: 0.2 1em;
     width: fit-content;
+    background-color: #f5f5f5;
+    padding: 0em;
+    margin: 0em;
+    padding: 4px;
 }
 
 .show {display:block;}
@@ -122,7 +115,7 @@ class Home extends View{
         <div style="margin: auto; overflow: hidden; position: fixed; width: 100%; margin-top: 0em; text-align: center; border-radius: 5px">
           <h1 style="font-family: arialBlack; color: red; font-size: 3em">IXNAY</h1>
           <h1>Manufacture for Autonomy</h1>
-          <div class="main-view riseUpHere" id="" style="margin:auto; box-shadow: rgb(17 17 18) 0px 7px 29px 0px ;     padding: 0.3em;  border-radius: 7px; width: 20vw; background-color: black;     height: 40em; width: 20em; ">
+          <div class="main-view riseUpHere" id="bodyBulk" style="margin:auto; box-shadow: rgb(17 17 18) 0px 7px 29px 0px ;     padding: 0.3em;  border-radius: 7px; width: 20vw; background-color: black;     height: 40em; width: 20em; ">
             <div id="stl_cont2" class="riseUpHere" style="width:auto; height:60vh ;     margin-top: 22vh !important; margin:0 auto; overflow: hidden; position: fixed !important; top: 3em; z-index: 9"></div>
 
             <div class="content" style="position:sticky; color: white;  z-index: 13; border-right: 10px red solid; width: 100%; border-radius: 5px 5px 0px 0px;  box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;  margin-top: 60vh ; padding: 1em  ;background-color: rgb(245 245 245); ">
@@ -130,12 +123,14 @@ class Home extends View{
                 <a href="/store/${this.props.store}"><i  class="fas fa-chevron-left"></i> Store</a>
                 <div id="container" style=" display: flex; margin-left: 2em;">
 
-                <div class="dropdown">
+                <div class="dropdown" style="margin-left: 9vw">
                   <i class="fas fa-ellipsis-v" style="color: black; padding: 0em 1em" onClick=${() => {{
                   /* When the user clicks on the button,
                   toggle between hiding and showing the dropdown content */
                   function myFunction() {
                     document.getElementById("myDropdown").classList.toggle("show");
+                    document.getElementById("bodyBulk").style.opacity = 0.8;
+
                   }
 
                   // Close the dropdown menu if the user clicks outside of it
@@ -154,7 +149,8 @@ class Home extends View{
 
                     }}}></i>
                   <div id="myDropdown" class="dropdown-content">
-                    <a href="#" id="container-one"></a>
+                    <a><i class="fas fa-save" style="font-size: 1.2em;  color: red;"></i></a>
+                    <a><i class="fas fa-save" style="font-size: 1.2em;  color: red;"></i></a>
                   </div>
                 </div>
                 </div>
@@ -163,8 +159,9 @@ class Home extends View{
               <h3 style="color: black" >All Yours</h3><hr/>
 
               <div style=";padding: 0.5em;border-radius: 5px; height: auto; width: fit-content; margin: auto; padding-bottom: 10em">
+              <div style="box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px; padding: 0.2em; border-radius: 5px">
               <canvas id="qr-code" style="align-content: center  ;"></canvas>
-
+              </div>
               </div>
               <iris-text style="display: none" id="modelDataRaw" user=${this.props.store} path="store/products/${this.props.product}/modelRaw"/>
             </div>
