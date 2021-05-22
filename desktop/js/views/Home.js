c19d3523b43f14ce50441c06a530a491b27e4111
Antony <allinto@icloud.com>
2021-05-22 18:57:44 +1200

    menu

--- desktop/js/views/Home.js
+++ desktop/js/views/Home.js
@@ -47,7 +47,7 @@ class Home extends View{
       console.log("run")
 
       var loc = document.getElementById("modelDataRaw").textContent
-      var anchor = ('<a download id="uploadBtn" href="' + loc + '" ><i class="fas fa-save" style="font-size: 1.5em;  color: #fff; margin-left: 3em"></i></a>');
+      var anchor = ('<a download id="uploadBtn" href="' + loc + '" ><i class="fas fa-save" style="font-size: 1.2em;  color: red;"></i></a>');
       $("#container-one").html(anchor)
 
     }      
@@ -77,6 +77,46 @@ class Home extends View{
   background: white;
   border-radius: 0px;
 }
+
+
+.dropbtn {
+  background-color: #f5f5f5;
+  color: white;
+  padding: 16px;
+  font-size: 16px;
+  border: none;
+  cursor: pointer;
+}
+
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+
+.dropdown-content a{
+  background-color: #f5f5f5;
+  padding: 0em;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f5f5f5;
+  z-index: 1;
+  left: 0em !important;
+  box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+}
+
+.dropdown-content a {
+  color: black;
+  text-decoration: none;
+  display: block;
+    padding: 0.2 1em;
+    width: fit-content;
+}
+
+.show {display:block;}
+
       </style>
       <div class="centered-container" style="margin: auto; margin-top: -5em">
         <div style="margin: auto; overflow: hidden; position: fixed; width: 100%; margin-top: 0em; text-align: center; border-radius: 5px">
@@ -87,9 +127,36 @@ class Home extends View{
 
             <div class="content" style="position:sticky; color: white;  z-index: 13; border-right: 10px red solid; width: 100%; border-radius: 5px 5px 0px 0px;  box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;  margin-top: 60vh ; padding: 1em  ;background-color: rgb(245 245 245); ">
               <div style="display: flex">
-                <a href="/store/${this.props.store}"><i class="fas fa-chevron-left"></i> Store</a>
+                <a href="/store/${this.props.store}"><i  class="fas fa-chevron-left"></i> Store</a>
                 <div id="container" style=" display: flex; margin-left: 2em;">
-                  <div id="container-one"></div>
+
+                <div class="dropdown">
+                  <i class="fas fa-ellipsis-v" style="color: black; padding: 0em 1em" onClick=${() => {{
+                  /* When the user clicks on the button,
+                  toggle between hiding and showing the dropdown content */
+                  function myFunction() {
+                    document.getElementById("myDropdown").classList.toggle("show");
+                  }
+
+                  // Close the dropdown menu if the user clicks outside of it
+                  window.onclick = function(event) {
+                    if (!event.target.matches('.dropbtn')) {
+                      var dropdowns = document.getElementsByClassName("dropdown-content");
+                      var i;
+                      for (i = 0; i < dropdowns.length; i++) {
+                        var openDropdown = dropdowns[i];
+                        if (openDropdown.classList.contains('show')) {
+                          openDropdown.classList.remove('show');
+                        }
+                      }
+                    }
+                  }
+
+                    }}}></i>
+                  <div id="myDropdown" class="dropdown-content">
+                    <a href="#" id="container-one"></a>
+                  </div>
+                </div>
                 </div>
               </div> 
 
