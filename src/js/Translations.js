9bfb5f4da9952d67e1faf8d880e433230e679ea7
Antony <allinto@icloud.com>
2021-06-24 21:37:26 +1200

    patch

--- src/js/Translations.js
+++ src/js/Translations.js
@@ -1,7 +1,7 @@
 export default {
   "en": {
     language_name: "English",
-    whats_your_name: "Pick a name",
+    whats_your_name: "What's your name?",
     new_user_go: "Go",
     already_have_an_account: "Already have an account?",
     back: "Back",
